365a910154c481aeadb666edd70d76e3c31a728c
Juan Carlos <jc.zr88@gmail.com>
2025-03-17 11:15:07 +0100

    configs2

--- android/app/src/main/java/com/example/app/MainActivity.java
+++ android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,5 +1,21 @@
 package com.example.app;
-
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Configurar el WebView para permitir contenido mixto
+    WebView webView = (WebView) findViewById(R.id.webview);
+    if (webView != null) {
+      WebSettings webSettings = webView.getSettings();
+      webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+    }
+  }
+}
+
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,42 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'Life Station',
-  webDir: 'src',
+  appName: 'hcontrol',
+  webDir: 'dist/hcontrol/browser',
+  plugins: {
+    server: {
+      androidScheme: 'https',
+      hostname: 'app.hcontrol.com'
+    },
+    CapacitorSQLite: {
+      iosDatabaseLocation: 'Library/CapacitorDatabase',
+      iosIsEncryption: true,
+      iosKeychainPrefix: 'angular-sqlite-app-starter',
+      iosBiometric: {
+        biometricAuth: false,
+        biometricTitle : "Biometric login for capacitor sqlite"
+      },
+      androidIsEncryption: true,
+      androidBiometric: {
+        biometricAuth : false,
+        biometricTitle : "Biometric login for capacitor sqlite",
+        biometricSubTitle : "Log in using your biometric"
+      },
+      electronIsEncryption: true,
+      electronWindowsLocation: "C:\\ProgramData\\CapacitorDatabases",
+      electronMacLocation: "/Volumes/Development_Lacie/Development/Databases",
+      electronLinuxLocation: "Databases"
+    },
+    Filesystem: {
+      permissions: {
+        publicStorage: {
+          description: 'Permiso necesario para guardar archivos en el almacenamiento público',
+        },
+      },
+      webUseUnstableCreateFile: true, // Habilitar la creación de archivos inestables en la web
+    },
+    ScreenOrientation: {}
+  },
 };
 
 export default config;
